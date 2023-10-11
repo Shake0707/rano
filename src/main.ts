fe1b530ee28eaf50fdbded6fa80bfa269b5b957c
Shake0707 <shaxriyorburiyev7@gmail.com>
2023-10-11 16:07:24 +0300

    update ver3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+window.addEventListener('resize', e => {
+    console.log((e.target as Window).screen.availWidth);
+});
+
 const header = document.querySelector('header') as HTMLHeadElement;
 
 document.addEventListener('scroll', () => {
-    if(window.scrollY === 0) {
+    if (window.scrollY === 0) {
         header.classList.remove('active');
     } else {
         header.classList.add('active');
